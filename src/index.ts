a61da2978cc27821e683791eb4e355e2bad94b21
Matt Lim <mlimcollege@gmail.com>
2021-07-22 23:24:33 -0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const main = async () => {
+  const app = express();
+
+  const port = Number(process.env.SERVER_PORT);
+  const host = process.env.SERVER_HOST as string;
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.listen(port, host, () => {
+    console.log(`Example app listening at http://${host}:${port}`);
+  });
+};
+
+main();
